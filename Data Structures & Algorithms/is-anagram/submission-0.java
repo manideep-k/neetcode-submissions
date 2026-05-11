ceaa7594d542247179a24318eff1298b976fbad5
Manideep Kethineedi <manideep.kethineedi@gmail.com>
2026-05-11 19:30:02 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,25 @@
+
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> sHash = new HashMap<>();
+        Map<Character, Integer> tHash = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            sHash.compute(c, (val, count) -> {
+                if (count == null) {
+                    return 1;
+                }
+                return count + 1;
+            });
+        }
+        for (char c : t.toCharArray()) {
+            tHash.compute(c, (val, count) -> {
+                if (count == null) {
+                    return 1;
+                }
+                return count + 1;
+            });
+        }
+        return sHash.equals(tHash);
+    }
+}
