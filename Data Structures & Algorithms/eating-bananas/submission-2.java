6b6fa5ac5c21599d7c5ed880d36f77ff43bb17c4
Manideep Kethineedi <manideep.kethineedi@gmail.com>
2026-06-28 14:19:32 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int L = 1;
+        int R = Arrays.stream(piles).max().getAsInt();
+        int minK = 0;
+        while (L <= R) {
+            int k = L + (R - L) / 2;
+            int guess = getEatingSpeed(piles, k);
+            if (guess <= h) {
+                minK = k;
+                R = k - 1;
+            } else {
+                L = k + 1;
+            }
+        }
+        return minK;
+    }
+    
+    private static int getEatingSpeed(int[] piles, int k) {
+        int hours = 0;
+        for (int pile : piles) {
+            hours = hours + Math.ceilDiv(pile, k);
+        }
+        return hours;
+    }
+}
